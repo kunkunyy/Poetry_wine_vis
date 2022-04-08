03c4fb885867e2913fcbb77e4baba3b4f2081b65
kunkunyy <1776572104@qq.com>
2022-04-08 16:39:02 +0800

    first push

---
+++ client/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  margin: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgb(245, 247, 159);
+}
+</style>
